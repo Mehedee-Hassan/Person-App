60380687553164de142d8acdfbd2b7ac925dd20c
Mehedee-Hassan <razin.meha@gmail.com>
2014-09-30 13:11:44 +0600

    version 1

---
+++ PersonApps/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonApps
+{
+    public partial class PersonForm : Form
+    {
+        public PersonForm()
+        {
+            InitializeComponent();
+        }
+
+        private Person newperson;
+        private void fullButton_Click(object sender, EventArgs e)
+        {
+            //newperson.FirstName = firstTextBox.Text;
+            //newperson.MiddleName = middleTextBox.Text;
+            //newperson.LastName = lastTextBox.Text;
+
+            if(middleTextBox.Text == "")
+                newperson = new Person(firstTextBox.Text , lastTextBox.Text);
+
+            else
+                newperson = new Person(firstTextBox.Text ,middleTextBox.Text ,lastTextBox.Text);
+
+
+            
+
+
+
+            fullTextBox.Text = newperson.getFullname();
+        }
+
+        private void reverseButton_Click(object sender, EventArgs e)
+        {
+            reverseTextBox.Text = newperson.getReverseName();
+        }
+    }
+}
---
+++ PersonApps/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonApps
+{
+    class Person
+    {
+        private string firstName;
+        private string middleName;
+        private string lastName;
+
+        public Person(string firstName, string middleName, string lastName):this(firstName , lastName)
+        {
+           
+            this.middleName = middleName;
+        
+        }
+
+
+        public Person(string firstName, string lastName)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+
+        }
+
+
+        public Person()
+        {
+            
+        }
+
+
+        public string getFullname()
+        {
+            return firstName + " " + middleName + " " + lastName;
+        }
+
+        public string getReverseName()
+        {
+            string fullName = getFullname();
+            char[] cArray = fullName.ToCharArray();
+           
+            string reverse = String.Empty;
+            for (int i = cArray.Length - 1; i > -1; i--)
+            {
+                reverse += cArray[i];
+            }
+             return reverse;
+            }
+
+
+        public string FirstName
+        {
+
+            get { return firstName; }
+            set { firstName = value; }
+        
+        }
+
+        public string MiddleName
+        {
+            get { return middleName; }
+            set { middleName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        
+           
+    }
+}
+
